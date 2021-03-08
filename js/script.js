c0a4e283a18d1470ba70f3b89e21eac1939b432b
Alex <example@gmail.com>
2021-03-08 18:42:05 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+//const category = 'toys';
+//console.log(`https://someurl.com/${category}/5`);
+
+
+//const category = 'films';
+//console.log(`https://films.ua/${category}/12/rembo/1985`);
+
+const user = 'Alex';
+alert(`Hi, ${user}`);
